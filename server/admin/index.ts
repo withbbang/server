85e140cd5542a738cfa1997642adc38edf4c1077
unknown <withbbang@gmail.com>
2023-03-23 19:25:45 +0900

    어드민 관련 라우터 삭제

--- server/admin/index.ts
+++ server/admin/index.ts
@@ -1,8 +1,6 @@
 import { Router } from 'express';
-import { adminInfo } from './adminInfo';
 import { categoryManage } from './categoryManage';
 
 export const admin: Router = Router();
 
-admin.use('/info', adminInfo);
 admin.use('/category-manage', categoryManage);
